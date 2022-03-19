761a99530e7dce4120911aebd74834869d8d6ea3
AkH <akh.buroscope@gmail.com>
2022-03-19 20:22:37 +0100

    JS : fix #1 fermeture modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,7 @@
+/*=============================================
+=                   MODAL                    =
+=============================================*/
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,17 +11,30 @@ function editNav() {
   }
 }
 
-// DOM Elements
+// Modal DOM Elements
 const modalbg = document.querySelector(".bground");
+const modalbg2 = document.querySelector(".bground2");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalCloseBtn = document.querySelectorAll(".close");
+const modalClose2Btn = document.querySelector(".close2");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalCloseBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+modalClose2Btn.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+function launchModal2() {
+  modalbg.style.display = "none";
+  modalbg2.style.display = "block";
+}
+
+function closeModal() {
+  modalbg.style.display = "none";
+  modalbg2.style.display = "none";
+}
 
